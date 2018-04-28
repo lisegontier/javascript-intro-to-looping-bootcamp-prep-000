eff5db8874edc0133a430c499481480668fa1a6b
Liz Gunter <midwestunicorn@gmail.com>
2018-04-28 18:30:09 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,7 +1,7 @@
 var i = 0;
 
 function forLoop(array) {
-    for (var i = 0; i < 25; i++) {
+    for (let i = 0; i < 25; i++) {
       if (i === 1) {
       return("I am 1 strange loop.");
     } else {
